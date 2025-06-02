2f98a733c72d2f8246f41b2ae3bdbbb58cf78f52
Marc Camps <marc.camps.garreta@gmail.com>
2025-06-02 22:08:56 +0200

    improve even more icons

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
-import { PanelRightOpen, PanelRightClose } from "lucide-react"
+import { AlignJustify, PanelRightOpen } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -276,7 +276,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      {state === "collapsed" ? <PanelRightClose /> :  <PanelRightOpen /> }
+      {state === "collapsed" ? <AlignJustify /> :  <PanelRightOpen /> }
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
